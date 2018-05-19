f8f78b72e9982df2d87d79be1edd12dbd0f0584d
root <root@MiguelC>
2018-05-19 16:52:20 -0300

    React Practice #1

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, '/src/components/index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|.jsx)$/,
+        loader: 'babel-loader',
+        exclude: '/node-modules/',
+        query: {
+          presets: ('es2015', 'react')
+        }
+      }, {
+        test: /(\.css)$/,
+        loader: 'style-loader'
+      }, {
+        test: /(\.css)$/,
+        loader: 'css-loader'
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 25000,
+          },
+        },
+      }
+    ]
+  }
+}
